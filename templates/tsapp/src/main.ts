b18b1ef7b2bd845ac7ca571e1aa508866652f3d5
drsh <41671372+drawmoon@users.noreply.github.com>
2022-05-30 13:51:09 +0800

    Update build script, and simple examples.

--- templates/tsapp/src/main.ts
+++ templates/tsapp/src/main.ts
@@ -1,1 +1,6 @@
-console.log('Hello, World!');
+import { IVersion } from './interfaces';
+import { Version } from './version';
+
+export function version(): IVersion {
+  return new Version(1, 0, 0);
+}
